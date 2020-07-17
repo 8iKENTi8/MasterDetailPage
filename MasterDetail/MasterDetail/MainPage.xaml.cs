d7d1ae9cea1a45c3c94394e74264b0d390edfcfb
Vladmir <roldugin.67@gmail.com>
2020-07-17 16:08:58 +0300

    Добавьте файлы проекта.

---
+++ MasterDetail/MasterDetail/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MasterDetail
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage(new News()) { 
+                BarBackgroundColor = Color.YellowGreen
+               
+            };
+
+            IsPresented = true;
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new News())
+            {
+                BarBackgroundColor = Color.YellowGreen
+
+            };
+
+            IsPresented = false;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Friends())
+            {
+                BarBackgroundColor = Color.FromHex("#00897b")
+            };
+
+            IsPresented = false;
+        }
+    }
+}
